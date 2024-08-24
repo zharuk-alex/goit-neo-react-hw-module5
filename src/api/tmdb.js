30056cf31a2b1be165fd83d52a247377e803cccf
zharuk.alex <zzharuk@gmail.com>
2024-08-24 09:47:53 +0300

    Readme.md, api_token

--- src/api/tmdb.js
+++ src/api/tmdb.js
@@ -2,7 +2,8 @@ import axios from "axios";
 axios.defaults.baseURL = "https://api.themoviedb.org/3";
 
 const VITE_API_TOKEN = import.meta.env.VITE_API_TOKEN;
-const VITE_API_KEY = import.meta.env.VITE_API_KEY;
+
+console.log(VITE_API_TOKEN);
 
 const options = {
   headers: {
